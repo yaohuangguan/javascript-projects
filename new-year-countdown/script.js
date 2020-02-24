b53a2adf8982f6c77d13b5b6842c200a1d4601d0
yaohuangguan <yaob@miamioh.edu>
2020-02-24 19:37:26 +0800

    d

--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -4,11 +4,11 @@ const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 const year = document.getElementById('year');
-const loading = document.getElementById('loading');
+
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+const fromTime = new Date(`February 13 ${currentYear} 00:00:00`);
 
 // Set background year
 year.innerText = currentYear + 1;
@@ -16,8 +16,8 @@ year.innerText = currentYear + 1;
 // Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
-  const diff = newYearTime - currentTime;
-
+  const diff = currentTime - fromTime
+  
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
@@ -32,7 +32,6 @@ function updateCountdown() {
 
 // Show spinner before countdown
 setTimeout(() => {
-  loading.remove();
   countdown.style.display = 'flex';
 }, 1000);
 
